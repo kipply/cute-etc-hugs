69e0a098a2257e5344d2a2cd674253a89f467da7
kipply <hello@carolchen.me>
2019-06-08 17:51:42 -0400

    ree

--- bot.py
+++ bot.py
@@ -275,7 +275,9 @@ def etf_arbitrage(exchange):
         break
     est_wfc /= float(temp)
 
-  except Exception as e: print(e)
+  except Exception as e: 
+    print(e)
+    return
   xlf_buy_est = (2 * est_wfc + 3 * est_ms + 2 * est_gs + 3 * est_bond) / 10.0
 
   print(xlf_buy_est, xlf_sell_estimate)
