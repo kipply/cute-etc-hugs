a04ca95e8030ded661afc03d9b3d13a53c833e8c
uraniumCoder <williamzhaoisawesome@gmail.com>
2019-06-09 06:27:04 +0800

    s

--- bot.py
+++ bot.py
@@ -231,7 +231,8 @@ def main():
             print(bcolors.WARNING + "OUT" + bcolors.ENDC)
             if(next_message['order_id'] not in filledID):
                 offer = trades[next_message['order_id']]
-                offering[offer['symbol']]['PENDING_' + offer['dir']] -= offer['size']
+                if(offer['type'] == "trade"):
+                    offering[offer['symbol']]['PENDING_' + offer['dir']] -= offer['size']
         elif next_message['type'] == "reject":
             print(trades[next_message['order_id']])
             print(next_message)
